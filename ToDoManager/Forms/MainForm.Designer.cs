2e39ceb1c854ea9ffede5705c8eee85b2aa9eaa6
Gabriel Galbavý <gabriel.galbavy9@gmail.com>
2025-10-11 15:49:23 +0200

    MainForm design update
    Simple UI design added, app shows current date

--- ToDoManager/Forms/MainForm.Designer.cs
+++ ToDoManager/Forms/MainForm.Designer.cs
@@ -28,19 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            HeadingPanel = new Panel();
+            MenuPanel = new Panel();
+            Datelabel = new Label();
+            HeadingPanel.SuspendLayout();
             SuspendLayout();
+            // 
+            // HeadingPanel
+            // 
+            HeadingPanel.BackColor = Color.FromArgb(90, 40, 216);
+            HeadingPanel.Controls.Add(Datelabel);
+            HeadingPanel.Dock = DockStyle.Top;
+            HeadingPanel.Location = new Point(0, 0);
+            HeadingPanel.Margin = new Padding(3, 2, 3, 2);
+            HeadingPanel.Name = "HeadingPanel";
+            HeadingPanel.Size = new Size(1133, 78);
+            HeadingPanel.TabIndex = 0;
+            // 
+            // MenuPanel
+            // 
+            MenuPanel.BackColor = Color.FromArgb(64, 34, 135);
+            MenuPanel.Dock = DockStyle.Left;
+            MenuPanel.Location = new Point(0, 78);
+            MenuPanel.Margin = new Padding(0);
+            MenuPanel.Name = "MenuPanel";
+            MenuPanel.Size = new Size(170, 574);
+            MenuPanel.TabIndex = 1;
+            // 
+            // Datelabel
+            // 
+            Datelabel.AutoSize = true;
+            Datelabel.ForeColor = SystemColors.HighlightText;
+            Datelabel.Location = new Point(16, 13);
+            Datelabel.Name = "Datelabel";
+            Datelabel.Size = new Size(168, 33);
+            Datelabel.TabIndex = 0;
+            Datelabel.Text = "Datelabel";
             // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(16F, 31F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.FromArgb(20, 7, 28);
+            ClientSize = new Size(1133, 652);
+            Controls.Add(MenuPanel);
+            Controls.Add(HeadingPanel);
+            Font = new Font("DejaVu Sans Mono", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            Margin = new Padding(7, 5, 7, 5);
             Name = "MainForm";
             Text = "MainForm";
             Load += MainForm_Load;
+            HeadingPanel.ResumeLayout(false);
+            HeadingPanel.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel HeadingPanel;
+        private Panel MenuPanel;
+        private Label Datelabel;
     }
 }
--- ToDoManager/Forms/MainForm.cs
+++ ToDoManager/Forms/MainForm.cs
@@ -1,5 +1,21 @@
 namespace ToDoManager
 {
+    enum Months
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
     public partial class MainForm : Form
     {
         public MainForm()
@@ -7,9 +23,16 @@ namespace ToDoManager
             InitializeComponent();
         }
 
+        private string ConstructDate()
+        {
+            string month = ((Months)DateTime.Now.Month - 1).ToString();
+            string date = @"";
+            return $@"{DateTime.Now.Day}th of {month}, {DateTime.Now.Year}";
+        }
+
         private void MainForm_Load(object sender, EventArgs e)
         {
-
+            Datelabel.Text = ConstructDate();
         }
     }
 }
